57767e96b54d0acc83f0c0f69a837fc4099c6850
themarkr <markrodriguezaa@gmail.com>
2022-08-09 13:34:09 -0400

    config vars

--- knexfile.js
+++ knexfile.js
@@ -10,9 +10,7 @@ module.exports = {
         connection: {
             database: process.env.DB_DATABASE,
             user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
+            password: process.env.DB_PASSWORD
         }
     },
 
@@ -34,18 +32,14 @@ module.exports = {
 
     production: {
         client: 'pg',
-        // connection: {
-        //     database: 'postgres://hfqbpqtdnzyrwx:31efe21f4a74591db7ce3f11dd860a6e26c3ef8a3a6d18058334eefca058a4b0@ec2-54-159-175-38.compute-1.amazonaws.com:5432/d5dir12t9jrq8p',
-        //     connectionString: process.env.DB_URL,
-        //     ssl: { rejectUnauthorized: false },
-
-        // },
-        connection: 'postgres://hfqbpqtdnzyrwx:31efe21f4a74591db7ce3f11dd860a6e26c3ef8a3a6d18058334eefca058a4b0@ec2-54-159-175-38.compute-1.amazonaws.com:5432/d5dir12t9jrq8p',
-        // pool: {
-        //     min: 2,
-        //     max: 10
-        // },
-        ssl: false,
+        connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
         migrations: {
             tableName: 'knex_migrations'
         }
