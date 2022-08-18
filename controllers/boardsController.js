e7e50ba5e849077a6787b3f4a8d51cd4182aa7a6
themarkr <markrodriguezaa@gmail.com>
2022-08-18 16:25:48 -0400

    create functions to get boards that belong to user, create board, edit and delete board

--- controllers/boardsController.js
+++ controllers/boardsController.js
@@ -1,4 +1,15 @@
 const Board = require('../models/Board');
+
+const userBoards = async(req, res) => {
+    const userId = req.params.userId;
+    try {
+        const response = await Board.searchBoards(userId);
+        res.status(201).json({ boards: response })
+    } catch (error) {
+        res.status(500).json({ message: `${error.message}` })
+    }
+}
+
 async function createBoard(req, res) {
     const { name, owner, color } = req.body;
 
@@ -10,6 +21,32 @@ async function createBoard(req, res) {
     }
 }
 
+const editBoard = async(req, res) => {
+    const { boardId, column } = req.params;
+    const newValue = req.body.newValue;
+
+    try {
+        const response = await Board.edit(boardId, column, newValue)
+        res.status(201).json({ board: response });
+    } catch (error) {
+        res.status(500).json({ message: `${error.message}` })
+    }
+}
+
+const deleteBoard = async(req, res) => {
+    const boardId = req.params.id;
+
+    try {
+        const response = await Board.delete(boardId);
+        res.status(203).json({ message: `Board with name ${response[0].name} has been successfully deleted` })
+    } catch (error) {
+        res.status(500).json({ message: `${error.message}` })
+    }
+}
+
 module.exports = {
-    createBoard
+    createBoard,
+    editBoard,
+    deleteBoard,
+    userBoards
 }
