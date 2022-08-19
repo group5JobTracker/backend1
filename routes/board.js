eadecc94e82dad18d69bf9b53c51de8828ad818c
themarkr <markrodriguezaa@gmail.com>
2022-08-18 22:23:09 -0400

    create boards route

--- routes/board.js
+++ routes/board.js
@@ -3,10 +3,16 @@ const boardController = require('../controllers/boardsController');
 
 router.get('/user/:userId', boardController.userBoards);
 
+router.get('/:boardId/cards', boardController.getCards);
+
 router.post('/create', boardController.createBoard);
+
+router.post('/addCard/:boardId', boardController.addToBoard)
 
 router.patch('/edit/:boardId/:column', boardController.editBoard)
 
 router.delete('/:id', boardController.deleteBoard)
+
+router.delete('/:boardId/removeCard/:appId', boardController.removeCard)
 
 module.exports = router;
